f34d17d0617e5af45df39968d8d55afa7fa5cdc4
Ehasan Ahmed <ehasan.ahmed01@gmail.com>
2025-03-21 15:03:58 +0600

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Icarus-afk.github.io/', // Add this line for GitHub Pages deployment
-})
